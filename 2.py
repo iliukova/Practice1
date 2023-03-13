8b0f20d5f6eea55ee94ec116228a0bc219cb64d2
Inna <innailiukova@gmail.com>
2023-03-13 18:57:41 +0300

    коментар

---
+++ 2.py
@@ -0,0 +1,8 @@
+# Збережіть імена декількох своїх друзів у список names
+# виведіть кожне з них по черзі
+
+names=["Olga","Lina","Sasha","Inna"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
